6a391619599cd1b4eae84d2807838aba2767884f
Hgehx <2446209451@qq.com>
2023-07-03 22:20:29 +0800

    面包屑

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import UserLogin from '@/views/user/Login.vue'
-
+// 管理员登录
 import Admin from '@/views/admin/Index.vue'
 import AdminLogin from '@/views/admin/login/Login.vue'
 import AdminHome from '@/views/admin/management/Home.vue'
+// 客房管理
+import AdminRoomIndex from '@/views/admin/management/room/Index.vue'
 import AdminRoom from '@/views/admin/management/room/Room.vue'
 import AdminRoomEdit from '@/views/admin/management/room/Edit.vue'
 import AdminRoomMore from '@/views/admin/management/room/More.vue'
+// 预约管理
 import AdminReservation from '@/views/admin/management/Reservation.vue'
+// 用户评价管理
 import AdminReview from '@/views/admin/management/Review.vue'
+// 管理员个人中心
 import AdminBasicInfo from '@/views/admin/management/myAcc/BasicInfo.vue'
 import AdminEditAvatar from '@/views/admin/management/myAcc/EditAvatar.vue'
 import AdminEditPassword from '@/views/admin/management/myAcc/EditPassword.vue'
@@ -24,6 +29,7 @@ const routes = [
   },
   {
     path: '/admin',
+    name: 'admin',
     component: Admin,
     meta: {
       title: '后台'
@@ -32,34 +38,50 @@ const routes = [
       {
         path: '', // 后台首页
         component: AdminHome,
+        name: 'home',
         meta: {
           title: '首页'
         }
       },
       {
-        path: 'room', // 客房管理
-        component: AdminRoom,
+        path: 'room', // 客房信息
+        component: AdminRoomIndex,
+        name: 'room',
         meta: {
-          title: '客房管理'
-        }
+          title: '客房信息'
+        },
+        children: [
+          {
+            path: '', // 客房管理
+            component: AdminRoom,
+            name: 'room',
+            meta: {
+              title: '客房管理'
+            }
+          },
+          {
+            path: 'roomedit', // 客房编辑
+            component: AdminRoomEdit,
+            name: 'room',
+            meta: {
+              title: '客房编辑'
+            }
+          },
+          {
+            path: 'rmore', // 更多客房内容
+            component: AdminRoomMore,
+            name: 'room',
+            meta: {
+              title: '更多客房内容'
+            }
+          }
+        ]
       },
-      {
-        path: 'roomedit', // 客房编辑
-        component: AdminRoomEdit,
-        meta: {
-          title: '客房编辑'
-        }
-      },
-      {
-        path: 'rmore', // 更多客房内容
-        component: AdminRoomMore,
-        meta: {
-          title: '更多客房内容'
-        }
-      },
+
       {
         path: 'resv', // 预定信息管理
         component: AdminReservation,
+        name: 'resv',
         meta: {
           title: '预定信息管理'
         }
@@ -67,6 +89,7 @@ const routes = [
       {
         path: 'revi', // 用户评价管理
         component: AdminReview,
+        name: 'revi',
         meta: {
           title: '用户评价管理'
         }
@@ -74,22 +97,25 @@ const routes = [
       {
         path: 'binfo', // 管理员基本信息
         component: AdminBasicInfo,
+        name: 'binfo',
         meta: {
           title: '管理员基本信息'
         }
       },
       {
-        path: 'eavatar', // 修改头像
-        component: AdminEditAvatar,
+        path: 'epassword', // 修改密码
+        component: AdminEditPassword,
+        name: 'epassword',
         meta: {
-          title: '修改头像'
+          title: '修改密码'
         }
       },
       {
-        path: 'epassword', // 修改密码
-        component: AdminEditPassword,
+        path: 'eavatar', // 修改头像
+        component: AdminEditAvatar,
+        name: 'eavatar',
         meta: {
-          title: '修改密码'
+          title: '修改头像'
         }
       }
     ]
---
+++ src/views/admin/management/room/Index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container"><router-view></router-view></div>
+</template>
+
+<script>
+export default { name: 'RoomIndex' }
+</script>
+
+<style lang="less" scoped></style>
